a9337be8b4986809414f67452f32e48765fec7d5
MCE893 <nikbagaturia@gmail.com>
2024-07-19 18:40:50 +0400

    first commit

---
+++ hello there/lesson1 (other project).py
@@ -0,0 +1,20 @@
+name ="Hello my name is: Nick"
+
+surname = "Baghaturia"
+
+age= "age: 16"
+
+height= "height: 172"
+knowledge="I know design"
+Knows="I don tknow cars"
+
+knows_graphicdesign= True
+Knows_cars=False
+
+#type((age))
+#type((name))
+#type((surname))
+#type((height))
+#type((knows_computer))
+#type((Knows_cars))
+print(name+ " " +surname+ " " +str(age)+ " "+height+ " "+ " "+ knowledge+ " "+ Knows)
---
+++ hello there/lesson1.py
@@ -0,0 +1,18 @@
+name ="Hello my name is: Nick"
+
+surname = "Baghaturia"
+
+age= "age16"
+
+height= "height172"
+
+knows_graphicdesign= True
+Knows_cars=False
+
+#type((age))
+#type((name))
+#type((surname))
+#type((height))
+#type((knows_computer))
+#type((Knows_cars))
+print(name+ " " +surname+ " " +str(age)+ " "+height+ " "+str(knows_graphicdesign)+ " " + str(Knows_cars))
