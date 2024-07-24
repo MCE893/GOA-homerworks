a13da106303ad06f3c38646f2c80a4f184069af9
MCE893 <nikbagaturia@gmail.com>
2024-07-24 20:55:30 +0400

    second commit

---
+++ day02/Homework/homework1.py
@@ -0,0 +1,8 @@
+Age=20
+year="2024=20 years old"
+print(year)
+Timetravel="I_want_to_go_in_year_3000"
+year="3000=996 years old"
+
+print(Timetravel)
+print(year)
---
+++ day02/lesson2.py/lesson2.py
@@ -0,0 +1,31 @@
+#true and false
+Computer_is_better_than_phone=True
+Phone_is_better_than_computer=False
+print(True and False)
+
+(5>8 and 9>10)
+print(False and False)
+
+(19>9 and 9>1)
+print(True and True)
+
+(20<19 and 8<5)
+print(False and True )
+
+#True or False
+
+Computer_is_better_than_phone=True
+Phone_is_better_than_computer=False
+
+print(True or False )
+
+(10>5 or 5>4)
+print(True or True)
+(6<4 or 7>5)
+print(True or False )
+(8>9 or 9>10 )
+print(False or False)
+
+(10<4 or 10>5)
+print(False or True)
+    
